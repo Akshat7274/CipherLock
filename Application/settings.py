0c2fc3f40d3e305b0390db1593b7f3b02409ad6a
Ze-Leus <71334938+Akshat7274@users.noreply.github.com>
2023-07-12 23:48:23 +0530

    Allowed Hosts Permission Update

--- Application/settings.py
+++ Application/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5kj5r0k$&&i$7+yx$4mip#u_+k3o$qin$k2#s-&--2$f=mopb%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
