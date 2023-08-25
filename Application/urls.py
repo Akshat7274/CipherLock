e6291dbf531d2c9c5634f5b902658e1c9a256193
Akshat7274 <akshat1205aj@gmail.com>
2023-08-26 01:50:58 +0530

    Final Vercel Related Deployment Configurations

--- Application/urls.py
+++ Application/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Main.urls')),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 echo "BUILD STARTED"
 python3.9 -m pip install -r requirements.txt
+echo "INSTALLATION COMPLETED... COLLECTING STATIC FILES"
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD FINISHED"
